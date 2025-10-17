206b26c7d1d70bd9e017b13cc40117cc88af32ef
Ely-Retr0 <elias090595@gmail.com>
2025-10-16 18:30:08 -0600

    Add files via upload

---
+++ RegInscripcionCarrera.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#define tam 2
+
+typedef struct Datospers{
+char nombre[20];
+char apeM[20];
+char apeP[20];
+}nomb;
+
+typedef struct domicilio{
+
+}
